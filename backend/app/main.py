52e073824a23415d9abfab4b1a73c624e49a9d32
Harriery <98697353+Harriery@users.noreply.github.com>
2026-07-13 00:48:37 +0200

    feat: integrate OpenAI API with FastAPI

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,15 @@
+import os
+from dotenv import load_dotenv
 from fastapi import FastAPI
 from pydantic import BaseModel
+from openai import OpenAI
 
+load_dotenv()
+api_key = os.getenv("OPENAI_API_KEY")
 app = FastAPI()
+client = OpenAI(api_key=api_key)
 class ChatRequest(BaseModel):
-    mesaj: str
+    message: str
 
 @app.get("/")
 def home():
@@ -11,5 +17,22 @@ def home():
 
 
 @app.post("/chat")
-def chat(request: ChatRequest):
-    return {"reply": f"You said: {request.mesaj}"}
+def chat(request: ChatRequest):         #request, kullanıcının gönderdiği JSON verisinin
+                                        #FastAPI tarafından ChatRequest modeline çevrilmiş halidir.
+                                        #request değişken adını biz verdik.
+                                        #ChatRequest ise verinin hangi yapıda olması gerektiğini belirtir.
+    message = request.message.strip()   # request.message yazınca kullanıcının body içinde gönderdiği message değerine ulaşıyoruz.
+                                        #def chat(data: ChatRequest):
+                                        #message = data.message
+                                        #Yani request özel bir Python kelimesi değil; anlamlı olduğu için seçtiğimiz parametre adı.
+    if message == "":
+        return {"reply": "Bu mesaj boş olamaz"}
+                                        # response = OpenAI’den dönen cevabın tamamını tutar.
+    response = client.responses.create( #OpenAI client üzerinden yeni bir cevap üretme isteği gönder.
+        model="gpt-5-mini",             #hangi modelin cevap üreteceğini seçer.
+        input=message,                  #kullanıcının yazdığı mesajı OpenAI’ye gönderir.
+    )
+
+    return {"reply": response.output_text}  #response.output_text == o cevabın içindeki metni alır.
+                                            # response bize sadece cevap gondermiyor, id, model, create_at, usage, output,output_text 
+                                            # ama bize sadece output_text lazim
